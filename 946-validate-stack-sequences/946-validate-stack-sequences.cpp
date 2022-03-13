c2c78e0d63f2792251a2cc61c64a29823ac8cfa7
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-03-13 22:23:24 +0800

    Time: 11 ms (67.22%), Space: 15.5 MB (16.21%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -13,7 +13,11 @@ public:
             }
             r = index;
         }
-        
+        int index = r;
+        while(stk.size() && index < n && popped[index] == stk.top()){
+            index++;
+            stk.pop();
+        }
         return stk.empty() && l == n && r == n;
     }
 };
