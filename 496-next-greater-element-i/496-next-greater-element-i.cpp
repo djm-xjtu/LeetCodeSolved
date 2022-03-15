4c89493cf888e88fdd6fd30f649fff377f283dba
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-03-15 16:14:36 +0800

    Time: 11 ms (43.82%), Space: 9 MB (31.92%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -5,23 +5,17 @@ public:
         int n = nums1.size(), m = nums2.size();
         vector<int> res(m,-1);
         vector<int> ans;
-        int f[n];
         unordered_map<int, int> cache;
-        for(int i = 0; i < n; i++){
-            cache[nums1[i]] = i+1;
-        }
-        for(int i = 0; i < m; i++){
-            if(cache[nums2[i]]) f[cache[nums2[i]]-1] = i;
-        }
         for(int i = 0; i < m; i++){
             while(stk.size() && nums2[stk.top()] < nums2[i]){
                 int cur = stk.top(); stk.pop();
-                res[cur] = nums2[i];
+                cache[nums2[cur]] = nums2[i]+1;
             }
             stk.push(i);
         }
         for(int i = 0; i < n; i++){
-            ans.push_back(res[f[i]]);
+            if(cache[nums1[i]] != 0) ans.push_back(cache[nums1[i]]-1);
+            else ans.push_back(-1);
         }
         return ans;
     }
