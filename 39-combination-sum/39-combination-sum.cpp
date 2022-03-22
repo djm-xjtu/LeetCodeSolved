4753e79e9569c1c880fed3555902aca15b71a4eb
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-03-22 16:10:29 +0800

    Time: 21 ms (36.78%), Space: 17.3 MB (31.42%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
+    vector<int> path;
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> path;
-        sort(candidates.begin(), candidates.end());
-        dfs(candidates, 0, 0, target, path, ans);
+        dfs(candidates, 0, 0, target);
         return ans;
     }
-    void dfs(vector<int>& candidates, int i, int sum, int target, vector<int>& path, vector<vector<int>>& ans){
+    void dfs(vector<int>& candidates, int i, int sum, int target){
         if(sum == target){
             ans.push_back(path);
             return;
         }
         if(i == candidates.size() || sum > target) return;
-        dfs(candidates, i + 1, sum, target, path, ans);
+        dfs(candidates, i+1, sum, target);
         path.push_back(candidates[i]);
-        dfs(candidates, i, sum + candidates[i], target, path, ans);
+        dfs(candidates, i, sum+candidates[i], target);
         path.pop_back();
     }
 };
