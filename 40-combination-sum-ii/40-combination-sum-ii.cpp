047fe18b2935e396d66bad1719bbb6bb0493ca2f
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-03-22 16:14:27 +0800

    Time: 8 ms (62.62%), Space: 13.7 MB (19.64%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
+    vector<int> path;
+    vector<vector<int>> ans;
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> path;
         sort(candidates.begin(), candidates.end());
-        dfs(candidates, 0, 0, target, path, ans);
+        dfs(candidates, 0, 0, target);
         return ans;
     }
-    void dfs(vector<int>& candidates, int i, int sum, int target, vector<int>& path, vector<vector<int>>& ans){
+    void dfs(vector<int>& candidates, int i, int sum, int target){
         if(sum == target){
             ans.push_back(path);
             return;
         }
         
         if(i == candidates.size() || sum > target) return;
+        
         for(int j = i + 1; j < candidates.size(); j++){
-            if(candidates[i] != candidates[j]){
-                dfs(candidates, j, sum, target, path, ans);
+            if(candidates[j] != candidates[i]){
+                dfs(candidates, j, sum, target);
                 break;
             }
         }
         
         path.push_back(candidates[i]);
-        dfs(candidates, i + 1, sum + candidates[i], target, path, ans);
+        dfs(candidates, i + 1, sum + candidates[i], target);
         path.pop_back();
+        
     }
 };
