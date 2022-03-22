856cbaea6210ea15fc909a68162e7a3bc55963b7
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-03-22 16:20:31 +0800

    Time: 3 ms (43.55%), Space: 6.4 MB (80.93%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
     vector<int> path;
+    vector<vector<int>> ans;
     vector<vector<int>> combinationSum3(int k, int n) {
-        dfs(1, 0, n, 0, k);
+        dfs(0, k, 1, 0, n);
         return ans;
     }
-    
-    void dfs(int i, int sum, int target, int count, int k){
-        if(sum > target) return;
-        if(k == count){
-            if(sum == target) ans.push_back(path);
+    void dfs(int count, int k, int i, int sum, int n){
+        if(sum == n && count == k){
+            ans.push_back(path);
             return;
         }
+        if(i > 9 || count == k || sum > n) return;
+        
         for(int j = i; j < 10; j++){
             path.push_back(j);
-            dfs(j + 1, sum + j, target, count + 1, k);
+            dfs(count + 1, k, j + 1, sum + j, n);
             path.pop_back();
         }
     }
