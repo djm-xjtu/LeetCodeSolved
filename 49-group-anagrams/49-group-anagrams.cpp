0f544e36e516baa6278a3e857a94b82d32137f6b
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-03-24 17:54:23 +0800

    Time: 34 ms (87.11%), Space: 18.4 MB (93.13%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -5,7 +5,7 @@ public:
         for(auto &str : strs){
             auto t = str;
             sort(t.begin(), t.end());
-            cache[t].push_back(move(str));
+            cache[t].push_back(move(str)); //move移动构造函数，移动完成后意味着str被删除了，作用是加速
         }
         vector<vector<string>> ans;
         for(auto i = cache.begin(); i != cache.end(); i++){
