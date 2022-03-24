1be72ab60d60f3fb600339ac7b936784e6190f97
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-03-24 21:22:11 +0800

    Time: 14 ms (27.71%), Space: 7.1 MB (88.30%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.cpp
@@ -5,20 +5,19 @@ public:
         string ans = "";
         for(int i = 0; i < n; i++){
             while(i < n && s[i] == ' ') i++;
-            if(i == n) break;
+            if(i == n) break; //这行很重要！！！！
             int j = i;
             while(j < n && s[j] != ' ') j++;
             string t = s.substr(i, j - i) + " ";
             ans.insert(0, t);
             i = j;
         }
-        auto itr = ans.begin();
-        for(int i = 0; i < ans.size(); i++){
-            if(ans[i] != ' ') break;
-            ans.erase(itr);
-            itr++;
-        }
+        
         ans.pop_back();
         return ans;
+        //解法1 见ac代码 21:17 每个单词自己翻转，翻转后放到字符串s的可能的最前面，最后删掉s的非单词部分再总体翻转
+        
+        //解法2 本代码 每次找到一个单词插入到ans的开头，最后取掉尾空格
+        
     }
 };
