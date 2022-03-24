00b9dd4003854503735852bbbb41470b1b40b04d
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-03-24 21:57:19 +0800

    Time: 4 ms (22.01%), Space: 6.3 MB (31.18%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -3,34 +3,19 @@ public:
     int compareVersion(string version1, string version2) {
         int n = version1.size(), m = version2.size();
         int i = 0, j = 0;
-        while(i < n && j < m){
-            if(i < n && version1[i] == '.') i++;
-            if(j < m && version2[j] == '.') j++;
+        while(i < n || j < m){
             int k1 = i, k2 = j;
-            while(i < n && version1[i] != '.') i++;
-            while(j < m && version2[j] != '.') j++;
-            string s1 = version1.substr(k1, i - k1);
-            string s2 = version2.substr(k2, j - k2);
-            int t1 = stoi(s1);
-            int t2 = stoi(s2);
+            while(k1 < n && version1[k1] != '.') k1++;
+            while(k2 < m && version2[k2] != '.') k2++;
+            string s1,s2;
+            if(k1 != i) s1 = version1.substr(i, k1 - i);
+            if(k2 != j) s2 = version2.substr(j, k2 - j);
+            int t1 = atoi(s1.c_str());
+            int t2 = atoi(s2.c_str());
             if(t1 < t2) return -1;
             else if(t1 > t2) return 1;
-        }
-        while(i < n){
-            if(i < n && version1[i] == '.') i++;
-            int k1 = i;
-            while(i < n && version1[i] != '.') i++;
-            string s1 = version1.substr(k1, i - k1);
-            int t1 = stoi(s1);
-            if(t1 > 0) return 1;
-        }
-        while(j < m){
-            if(j < m && version2[j] == '.') j++;
-            int k2 = j;
-            while(j < m && version2[j] != '.') j++;
-            string s2 = version2.substr(k2, j - k2);
-            int t2 = stoi(s2);
-            if(t2 > 0) return -1;
+            if(k1 != i) i = k1 + 1;
+            if(k2 != j) j = k2 + 1;
         }
         return 0;
     }
