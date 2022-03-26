fef7f40c31bbb50e42b86fc1fd4796c155421c82
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-03-26 11:33:42 +0800

    Time: 9 ms (17.85%), Space: 10.1 MB (71.71%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         int l = 0, r = nums.size()-1;
         while(l < r){
             int mid = l + r >> 1;
-            if(nums[mid] >= nums[0]) l = mid + 1;
+            if(nums[mid] >= nums[0]) l = mid + 1; // 因为要取到第一个绝对小于nums[0]的数
             else r = mid;
         }
         return nums[l];
