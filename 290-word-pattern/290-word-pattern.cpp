d1e1813ff385802355b9bf4a771a76fc672e627c
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-04-07 21:10:17 +0800

    Time: 0 ms (100.00%), Space: 6.4 MB (52.03%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        stringstream raw(str);
+        vector<string> strs;
+        string line;
+        while (raw >> line) strs.push_back(line);
+        if (pattern.size() != strs.size()) return false;
+        unordered_map<char, string> PS;
+        unordered_map<string, char> SP;
+        for (int i = 0; i < pattern.size(); i ++ )
+        {
+            if (PS.count(pattern[i]) == 0) PS[pattern[i]] = strs[i];
+            if (SP.count(strs[i]) == 0) SP[strs[i]] = pattern[i];
+            if (PS[pattern[i]] != strs[i]) return false;
+            if (SP[strs[i]] != pattern[i]) return false;
+        }
+        return true;
+    }
+};
