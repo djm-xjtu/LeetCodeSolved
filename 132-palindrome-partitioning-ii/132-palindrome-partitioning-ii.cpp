a4c064f905e145cd412d89a6f27f2c8941b17157
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-04-11 19:45:24 +0800

    Time: 61 ms (73.62%), Space: 10.9 MB (58.71%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -3,7 +3,8 @@ public:
     int minCut(string s) {
         int n = s.size();
         s = ' ' + s;
-        vector<vector<bool>> dp(n + 1, vector<bool>(n + 1, 0));
+        bool dp[2010][2010]; //dp[i][j]表示i-j是不是回文串
+        memset(dp, 0, sizeof dp);
         for(int j = 1; j <= n; j++){
             for(int i = 1; i <= j; i++){
                 if(s[i] == s[j])
@@ -11,7 +12,7 @@ public:
             }
         }
 
-        vector<int> f(n + 1, 10000);
+        vector<int> f(n+1, 100000);//f[i]表示到0～i的串都是分为回文串的数量，因此最后要-1
         f[0] = 0;
         for(int i = 1; i <= n; i++){
             for(int k = 1; k <= i; k++){
