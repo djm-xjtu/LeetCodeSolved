8ab8ea8861509dac83ce2508993e62dfaea17dd9
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-04-11 19:54:06 +0800

    Time: 75 ms (67.61%), Space: 10.9 MB (58.71%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     int minCut(string s) {
         int n = s.size();
-        s = ' ' + s;
-        bool dp[2010][2010]; //dp[i][j]表示i-j是不是回文串
-        memset(dp, 0, sizeof dp);
+        bool vis[2010][2010];
+        memset(vis, 0, sizeof vis);
+        for(int j = 0; j < n; j++){
+            for(int i = 0; i <= j; i++){
+                if(s[i] == s[j])
+                    if(i+1 > j-1 || vis[i+1][j-1]) vis[i][j] = 1;
+            }
+        }
+        vector<int> f(n+1, 100000);
+        f[0] = 0;
         for(int j = 1; j <= n; j++){
             for(int i = 1; i <= j; i++){
-                if(s[i] == s[j])
-                    if(i + 1 > j - 1 || dp[i+1][j-1]) dp[i][j] = 1;
+                if(vis[i-1][j-1]) f[j] = min(f[j], f[i-1] + 1);
             }
         }
-
-        vector<int> f(n+1, 100000);//f[i]表示到0～i的串都是分为回文串的数量，因此最后要-1
-        f[0] = 0;
-        for(int i = 1; i <= n; i++){
-            for(int k = 1; k <= i; k++){
-                if(dp[k][i]) f[i] = min(f[i], f[k-1] + 1);
-            }
-        }
-
         return f[n] - 1;
     }
 };
