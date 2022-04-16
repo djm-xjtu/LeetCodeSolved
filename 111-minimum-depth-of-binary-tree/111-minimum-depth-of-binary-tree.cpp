d6b45bc996c0c568f906ab9eac2d8ea20ae599ba
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-04-16 11:41:16 +0800

    Time: 285 ms (78.65%), Space: 144.7 MB (71.57%) - LeetHub

--- 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
+++ 111-minimum-depth-of-binary-tree/111-minimum-depth-of-binary-tree.cpp
@@ -16,6 +16,6 @@ public:
         int l = minDepth(root->left);
         int r = minDepth(root->right);
         if(l == 0 || r == 0) return l + r + 1;
-        return min(l,r) + 1;
+        return min(l, r) + 1;
     }
 };
