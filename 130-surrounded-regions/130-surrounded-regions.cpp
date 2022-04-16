3195100dd351063f82ac69b897c90ac89e5f3b3b
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-04-16 21:46:42 +0800

    Time: 29 ms (15.08%), Space: 10.4 MB (26.25%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,52 +1,45 @@
 class Solution {
 public:
-    int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, -1, 0, 1};
-    
-    void bfs(vector<vector<char>>& board, int x, int y){
-        vector<pair<int, int>> res;
-        bool vis[210][210];
-        memset(vis, 0, sizeof vis);
-        queue<pair<int, int>> q;
-        q.push({x, y});
-        res.push_back({x, y});
-        vis[x][y] = 1;
-        bool f = 0;
-        while(q.size()){
-            auto t = q.front(); q.pop();
-            for(int i = 0; i < 4; i++){
-                int nx = t.first + dx[i], ny = t.second + dy[i];
-                if(nx < 0 || nx >= board.size() || ny < 0 || ny >= board[0].size()) {
-                    f = 1;
-                    break;
-                }
-                if(!vis[nx][ny] && board[nx][ny] == 'O'){
-                    q.push({nx, ny});
-                    res.push_back({nx, ny});
-                    vis[nx][ny] = 1;
-                }
-            }
-            if(f) break;
-        }
-        if(!f){
-            for(auto t : res){
-                board[t.first][t.second] = 'X';
-            }
-        }
-    }
-    // OXXOX  OXXOX
-    // XOOXO  XXXXO
-    // XOXOX  XXXOX
-    // OXOOO  OXOOO
-    // XXOXO  XXOXO
+    vector<vector<bool>> st;
+    int n, m;
+
     void solve(vector<vector<char>>& board) {
-        int n = board.size(), m = board[0].size();
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(board[i][j] == 'O'){
-                    bfs(board, i, j);
-                }
-            }
+        if (board.empty()) return;
+        n = board.size(), m = board[0].size();
+        for (int i = 0; i < n; i ++ )
+        {
+            vector<bool> temp;
+            for (int j = 0; j < m; j ++ )
+                temp.push_back(false);
+            st.push_back(temp);
         }
-        return;
+
+        for (int i = 0; i < n; i ++ )
+        {
+            if (board[i][0] == 'O') dfs(i, 0, board);
+            if (board[i][m - 1] == 'O') dfs(i, m - 1, board);
+        }
+        for (int i = 0; i < m; i ++ )
+        {
+            if (board[0][i] == 'O') dfs(0, i, board);
+            if (board[n - 1][i] == 'O') dfs(n - 1, i, board);
+        }
+
+        for (int i = 0; i < n; i ++ )
+            for (int j = 0; j < m; j ++ )
+                if (!st[i][j])
+                    board[i][j] = 'X';
+    }
+
+    void dfs(int x, int y, vector<vector<char>>&board)
+    {
+        st[x][y] = true;
+        int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+        for (int i = 0; i < 4; i ++ )
+        {
+            int a = x + dx[i], b = y + dy[i];
+            if (a >= 0 && a < n && b >= 0 && b < m && !st[a][b] && board[a][b] == 'O')
+                dfs(a, b, board);
+        }
     }
 };
