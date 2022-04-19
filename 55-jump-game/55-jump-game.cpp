32aa7f072a15b0371729f712c68fe8a7c85a45ed
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-04-19 21:52:01 +0800

    Time: 81 ms (66.87%), Space: 48.4 MB (38.64%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -4,10 +4,8 @@ public:
         int n = nums.size();
         int last = 0;
         for(int i = 1; i < nums.size(); i++){
-            while(last < i && i > last + nums[last]){
-                last++;
-            }
-            if(last == i) return 0;
+            while(last < i && last + nums[last] < i) last++; //枚举每个点的最远跳法
+            if(last == i) return 0; // i前所有点都挑不到i
         }
         return 1;
     }
