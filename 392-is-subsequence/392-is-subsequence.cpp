1f93527fe14ee9bb8a8abef3239068b96e1fc0ca
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-04-19 21:27:20 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (88.22%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.cpp
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -2,20 +2,12 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int n = s.size(), m = t.size();
-        s = " " + s;
-        t = " " + t;
         if(n > m) return 0;
-        int f[n+1][m+1];
-        memset(f, 0, sizeof f);
-        int res = 0;
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                f[i][j] = max(f[i-1][j], f[i][j-1]);
-                if(s[i] == t[j]) f[i][j] = max(f[i][j], f[i-1][j-1] + 1);
-                res = max(res, f[i][j]);
-            }
+        int idx = 0;
+        for(int i = 0; i < m; i++){
+            if(s[idx] == t[i]) idx++;
         }
-        if(res == n) return 1;
+        if(idx == n) return 1;
         return 0;
     }
 };
