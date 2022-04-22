4e9474b6ee287fa46d510ac2559809520d8e5d49
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-04-22 11:13:32 +0800

    Time: 85 ms (10.02%), Space: 18.7 MB (45.70%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
@@ -3,8 +3,8 @@ public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         set<long> st;
         for (int i = 0; i < nums.size(); i++) {
-            auto lb = st.lower_bound((long)nums[i] - t);
-            if (lb != st.end() && *lb <= (long)nums[i] + t) return 1;
+            auto l = st.lower_bound((long)nums[i] - t);
+            if(l != st.end() && *l <= (long)nums[i] + t) return 1;
             st.insert(nums[i]);
             if (i >= k) st.erase(nums[i - k]);
         }
