f9b2ba2304b9222d701c279907e4ff3f9522bf0c
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-05-17 12:05:03 +0800

    Time: 27 ms (98.84%), Space: 19.6 MB (72.09%) - LeetHub

---
+++ 2122-recover-the-original-array/2122-recover-the-original-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> recoverArray(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < n; i++){
+            if(nums[i] == nums[i-1]) continue;
+            int d = nums[i] - nums[0];
+            if(d == 0 || d & 1 > 0) continue;
+            int k = d / 2;
+            vector<bool> vis(n);
+            vis[i] = true;
+            vector<int> ans{(nums[0] + nums[i]) / 2};
+            int l = 1, r = i + 1;
+            while(r < n){
+                while(l < n && vis[l]) l++;
+                while(r < n && nums[r] - nums[l] < 2 * k) r++;
+                if(r == n || nums[r] - nums[l] > 2 * k) break;
+                vis[r] = true;
+                ans.push_back((nums[l] + nums[r]) / 2);
+                l++, r++;
+            }
+            if(ans.size() == n / 2) return ans;
+        }
+        return {};
+    }
+};
