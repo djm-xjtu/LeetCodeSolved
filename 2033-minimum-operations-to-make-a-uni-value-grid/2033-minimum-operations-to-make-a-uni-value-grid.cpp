1fbbe13a847615d7157e423953128a36dc2489ef
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-05-19 11:41:13 +0800

    Time: 329 ms (68.05%), Space: 83 MB (85.12%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
+        int n = grid.size() * grid[0].size(), sum = 0;
         vector<int> g;
-        for(auto st : grid)
-            for(auto t : st)
+        for(auto& st : grid){
+            for(auto& t : st){
+                if((t - grid[0][0]) % x != 0) return -1;
                 g.push_back(t);
-        sort(g.begin(), g.end());
-        //保证每个数整除x一致
-        int k = g[0] % x, sum = 0;
-        int n = g.size();
-        for(int i = 0; i < n; i++){
-            if(g[i] % x != k) return -1;
-            //和前面类似的题一样采取到达中点的贪心思想
-            //前面题已经得到结论，odd and even是一样的
-            if(n - 1 - i > i){
-                sum += (g[n - i - 1] - g[i]) / x;
             }
+        }
+        sort(g.begin(), g.end());
+        for(auto& st : g){
+            sum += abs(st - g[n/2]) / x;
         }
         return sum;
     }
