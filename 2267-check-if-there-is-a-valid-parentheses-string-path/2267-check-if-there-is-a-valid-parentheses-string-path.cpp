7ceabe5fd7a39ec5daa3fba914ff88e6133d5eb6
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-05-24 18:34:43 +0800

    Time: 1020 ms (16.78%), Space: 122.6 MB (27.26%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     bool hasValidPath(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        if(grid[0][0] == ')') return false;
-        vector<vector<vector<bool>>> f;
-        for (int i = 0; i < n; i++) {
-            f.push_back(vector<vector<bool>>());
-            for (int j = 0; j < m; j++) f.back().push_back(vector<bool>(n + m));
-        }
-        f[0][0][1] = 1;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(i == 0 && j == 0) continue;
-                int t = (grid[i][j] == '(' ? 1 : -1);
-                for(int k = 0; k < n + m; k++){
-                    int pre = k - t;
-                    if(pre < 0 || pre >= n + m) continue;
-                    if(i > 0) f[i][j][k] = f[i][j][k] || f[i-1][j][pre];
-                    if(j > 0) f[i][j][k] = f[i][j][k] || f[i][j-1][pre];
-                }
+        int dx[2] = {0, 1}, dy[2] = {1, 0};
+        if(grid[0][0] == ')' || grid[n-1][m-1] == '(') return false;
+        vector<vector<vector<bool>>> vis(n, vector<vector<bool>>(m, vector<bool>(n + m, 0)));
+        queue<tuple<int, int, int>> q;
+        q.emplace(0, 0, 1);
+        vis[0][0][0] = 1;
+        while(q.size()){
+            auto [x, y, d] = q.front();
+            q.pop();
+            for(int i = 0; i < 2; i++){
+                int nx = x + dx[i], ny = y + dy[i];
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(vis[nx][ny][d]) continue;
+                //不要修改状态d，在加入队列时修改，不然会影响其他方向
+                int t = (grid[nx][ny] == '(' ? 1 : -1);
+                if(nx == n - 1 && ny == m - 1 && d + t == 0) return true;
+                if(d + t >= 0) q.emplace(nx, ny, d + t), vis[nx][ny][d] = true;
             }
         }
-        return f[n-1][m-1][0];
+        return false;
     }
 };
