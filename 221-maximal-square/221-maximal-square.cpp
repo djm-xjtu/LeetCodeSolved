89a964998b85ca3eeb14d9b2fc83830f88900f96
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-05-25 15:56:04 +0800

    Time: 99 ms (34.50%), Space: 18 MB (64.54%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        int f[310][310];
-        memset(f, 0, sizeof f);
-        int n = matrix.size();
-        if(n == 0) return 0;
-        int m = matrix[0].size();        
-        int ans = 0;
+    int maximalSquare(vector<vector<char>>& ma) {
+        int n = ma.size(), m = ma[0].size();
+        vector<vector<int>> f(n, vector<int>(m, 0));
+        int max_val = 0;
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                int u = i + 1, v = j + 1;
-                if(matrix[i][j] == '1') {
-                    f[u][v] = min(f[u-1][v-1], min(f[u][v-1], f[u-1][v])) + 1;
-                    ans = max(ans, f[u][v] * f[u][v]);
-                }
-                cout << f[u][v] << " ";
+                if(ma[i][j] == '1') f[i][j] = 1, max_val = max(max_val, 1);
+                if(i < 1 || j < 1 || ma[i][j] == '0') continue;
+                int t = min(min(f[i-1][j], f[i][j-1]), f[i-1][j-1]);
+                f[i][j] = t + 1;
+                max_val = max(max_val, f[i][j] * f[i][j]);
             }
-            cout << "\n";
         }
-        
-        return ans;
+        return max_val;
     }
 };
