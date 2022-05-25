431b04e40a92df4b5cc04aa308f14ec5d33f1b6c
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-05-25 18:19:15 +0800

    Time: 158 ms (90.81%), Space: 55.9 MB (16.09%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     long long maxRunTime(int n, vector<int>& b) {
-        long long sum = accumulate(b.begin(), b.end(), 1ll*0);
-        auto check = [&](long long t) {
-            long long sumt = 0;
-            for(int i : b) sumt += min(t, (long long)i);
-            return sumt / t >= n;
+        using ll = long long;
+        ll sum = accumulate(b.begin(), b.end(), 1ll*0);
+        auto check = [&](ll t){
+            ll sumt = 0;
+            for(int i : b) sumt += min(t, 1ll*i);
+            return sumt >= t * n;
         };
-        long long l = 0, r = sum / n;
+        ll l = 0, r = sum / n;
         while(l < r){
-            long long mid = l + r + 1>> 1;
+            ll mid = l + r + 1 >> 1;
             if(check(mid)) l = mid;
             else r = mid - 1;
         }
