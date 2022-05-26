69528040195722105c0dcded98f29029b8e4d18b
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-05-26 16:51:00 +0800

    Time: 35 ms (88.13%), Space: 12.6 MB (9.38%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -9,8 +9,11 @@ public:
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 for(int k = 1; k <= K && k <= i; k++){
+                    //从不用第j个数直接转移
                     f[i][j][k] = (f[i][j-1][k] + f[i][j][k]) % mod;
+                    //在i-1的状态中，代价+1，增加一个j
                     f[i][j][k] = (f[i][j][k] + f[i-1][j-1][k-1]) % mod;
+                    //在i-1的状态中，代价不变，并在j没有产生代价的方案中增加（1～j）一共j种方案
                     f[i][j][k] = (f[i][j][k] + (f[i-1][j][k] - f[i-1][j-1][k] + mod) * j) % mod;
                 }
             }
