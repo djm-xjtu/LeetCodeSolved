bc9e762a9b42973f530a04ca259f50fff9b1ce18
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-05-27 16:39:20 +0800

    Time: 46 ms (72.93%), Space: 18.7 MB (51.57%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        set<long> st;
-        for (int i = 0; i < nums.size(); i++) {
-            auto l = st.lower_bound((long)nums[i] - t);
-            if(l != st.end() && *l <= (long)nums[i] + t) return 1;
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) 
+    {
+        int n = nums.size();
+        multiset<long> st;
+        for(int i = 0; i < n; i++){
+            //把区间左边多余的数删去
+            if(i > k) st.erase(st.lower_bound(1l*nums[i-k-1]));//如果写st.erase(1l*nums[i-k-1])会删掉所有这个值的数
+            //找是否有[nums[i] - t, nums[i] + t]的数
+            auto iter = st.lower_bound(1l*nums[i] - t);
+            if(iter != st.end() && abs(*iter - 1l*nums[i]) <= t) return true;
             st.insert(nums[i]);
-            if (i >= k) st.erase(nums[i - k]);
         }
-        return 0;
+        return false;
     }
 };
