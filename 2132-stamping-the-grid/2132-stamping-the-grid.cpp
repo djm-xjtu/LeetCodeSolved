16cba0c86500f6a1c1c6b613b8cc3c52b901b912
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-05-28 17:34:43 +0800

    Time: 695 ms (67.76%), Space: 206.2 MB (11.84%) - LeetHub

--- 2132-stamping-the-grid/2132-stamping-the-grid.cpp
+++ 2132-stamping-the-grid/2132-stamping-the-grid.cpp
@@ -14,6 +14,7 @@ public:
                 int x = i + stampHeight, y = j + stampWidth;
                 // 求[x1, y1] 到 [x2, y2]范围的和
                 // sum[x2][y2] - sum[x2][y1 - 1] - sum[x1 - 1][y2] + sum[x1 - 1][x2 - 1]
+                // 如果能盖就全盖上
                 if (x <= m && y <= n && sum[x][y] - sum[x][j] - sum[i][y] + sum[i][j] == 0) {
                     ++diff[i][j];
                     --diff[i][y];
@@ -22,17 +23,17 @@ public:
                 }
             }
         }
-
-        vector<int> cnt(n + 1), pre(n + 1);
+        // 从查分数组中恢复原数组
+        vector<vector<int>> cnt(m + 1, vector<int>(n + 1));
+        
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                //cnt[j+1] - cnt[j] = pre[j+1] - pre[j] + diff[i][j]
-                cnt[j + 1] = cnt[j] + pre[j + 1] - pre[j] + diff[i][j];
-                if (cnt[j + 1] == 0 && grid[i][j] == 0) {
+                //也可以用滚动数组: cnt[j+1] - cnt[j] = pre[j+1] - pre[j] + diff[i][j]
+                cnt[i + 1][j + 1] = cnt[i + 1][j] + cnt[i][j + 1] - cnt[i][j] + diff[i][j];
+                if (cnt[i + 1][j + 1] == 0 && grid[i][j] == 0) {
                     return false;
                 }
             }
-            swap(cnt, pre);
         }
 
         return true;
