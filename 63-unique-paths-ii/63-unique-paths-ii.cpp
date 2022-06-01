fdc2317ee56efbcffd4f256c5a691186e4a6439c
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-06-01 20:39:06 +0800

    Time: 7 ms (33.77%), Space: 7.5 MB (90.21%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size(), m = obstacleGrid[0].size();
-        if(!n || !m) return 0;
-        int f[110][110];
+        int f[n][m];
         memset(f, 0, sizeof f);
         f[0][0] = 1 - obstacleGrid[0][0];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(obstacleGrid[i][j] == 1) f[i][j] = 0;
                 else{
-                    if(i - 1 >= 0) f[i][j] += f[i-1][j];
-                    if(j - 1 >= 0) f[i][j] += f[i][j-1];
+                    if(i > 0) f[i][j] += f[i-1][j];
+                    if(j > 0) f[i][j] += f[i][j-1];
                 }
             }
         }
