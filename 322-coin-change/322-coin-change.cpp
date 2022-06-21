4727348eaabcaa2314cfa66921719858571a9240
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-06-21 19:58:04 +0800

    Time: 67 ms (91.94%), Space: 10.1 MB (92.73%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        int f[10010];
-        memset(f, 0x3f, sizeof f);
+        int f[amount+1], n = coins.size();
+        for(int i = 0; i <= amount; i++)
+            f[i] = INT_MAX / 2;
         f[0] = 0;
-        for(int i = 0; i < coins.size(); i++){
+        for(int i = 0; i < n; i++){
             for(int j = coins[i]; j <= amount; j++){
-                f[j] = min(f[j], f[j - coins[i]] + 1);
+                f[j] = min(f[j], f[j-coins[i]] + 1);
             }
         }
-        if(f[amount] == 0x3f3f3f3f) return -1;
-        else return f[amount];
+        return f[amount] == INT_MAX / 2 ? -1 : f[amount];
     }
 };
