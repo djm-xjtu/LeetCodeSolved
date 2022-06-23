d48882d9f405148dba2a0c39d7249170bda39b02
Daniel <72679543+djm-xjtu@users.noreply.github.com>
2022-06-23 17:11:43 +0800

    Time: 62 ms (83.60%), Space: 10.4 MB (53.43%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -2,21 +2,31 @@ class Solution {
 public:
     int minCut(string s) {
         int n = s.size();
-        bool vis[2010][2010];
+        bool vis[n][n];
         memset(vis, 0, sizeof vis);
-        for(int j = 0; j < n; j++){
-            for(int i = 0; i <= j; i++){
-                if(s[i] == s[j])
-                    if(i+1 > j-1 || vis[i+1][j-1]) vis[i][j] = 1;
+        for(int len = 1; len <= n; len++){
+            for(int i = 0; i <= n - len; i++){
+                int j = i + len - 1;
+                if(s[i] == s[j]){
+                    if(i + 1 >= j - 1)
+                        vis[i][j] = 1;
+                    else
+                        vis[i][j] = vis[i+1][j-1];
+                }
             }
         }
-        vector<int> f(n+1, 100000);
-        f[0] = 0;
-        for(int j = 1; j <= n; j++){
-            for(int i = 1; i <= j; i++){
-                if(vis[i-1][j-1]) f[j] = min(f[j], f[i-1] + 1);
+        vector<int> f(n, INT_MAX/2);
+        f[0] = 1;
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                if(vis[j][i]){
+                    if(j == 0)
+                        f[i] = 1;
+                    else
+                        f[i] = min(f[i], f[j-1] + 1);
+                }
             }
         }
-        return f[n] - 1;
+        return f[n-1] - 1;
     }
 };
