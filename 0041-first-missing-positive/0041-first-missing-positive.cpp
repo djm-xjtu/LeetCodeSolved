f881f36080d82703321e919e1124817940fce3e8
Jiaming Deng <72679543+djm-xjtu@users.noreply.github.com>
2022-10-19 23:15:10 +0100

    Time: 103 ms (50.88%), Space: 31.5 MB (40.76%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(nums[i] <= 0) nums[i] = INT_MAX;
+        }
+        for(int i = 0; i < n; i++){
+            if(abs(nums[i]) <= n && nums[abs(nums[i])-1] > 0){
+                nums[abs(nums[i])-1] *= -1;
+            }
+        }
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0) return i + 1;
+        }
+        return n + 1;
+    }
+};
